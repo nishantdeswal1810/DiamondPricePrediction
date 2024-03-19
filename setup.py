c84b480d85c769f427a2f4276f18482fcb8e67a3
nishantdeswal1810 <msa23010@iiitl.ac.in>
2024-03-19 22:59:40 +0530

    created pipelines

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import find_packages,setup
 from typing import List
 
+HYPEN_E_DOT='-e .'
+
 def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n","") for req in requirements]
 
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
         return requirements
 
 
@@ -14,8 +18,9 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name='DiamondPriceprediction',
     version='0.0.1',
-    author='Nishant',
-    author_email='nishantdeswal702@gmail.com',
+    author='Pwskills',
+    author_email="example@gmail.com",
     install_requires=get_requirements('requirements.txt'),
     packages=find_packages()
+
 )
